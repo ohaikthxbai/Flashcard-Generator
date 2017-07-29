599381ac608ea18baec9e16d66d3f131bbe525e7
Justin G <MBA13-JG@Justins-MacBook-Air.local>
2017-07-29 15:11:40 -0500

    cloze deck function added

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var clozeCard = require('./ClozeCard');
 var bCard1 = new basicCard("How many stars are on the flag of the United States?", "50");
 //testing the basic cards
 //console.log(test);
-var bCard2 = new basicCard("How many stripes are on the flag of the United State?", "13");
+var bCard2 = new basicCard("How many stripes are on the flag of the United States?", "13");
 //console.log(test2);
 
 var basicQ = [bCard1.front, bCard2.front];
@@ -16,11 +16,11 @@ var basicA = [bCard1.back, bCard2.back];
 //console.log(basicAnswer[1]);
 
 // Cloze Cards
-var clozeTest = new clozeCard("Phoebe is the name of my cat", "Phoebe");
-var clozeTest2 = new clozeCard("Springfield is the capital of Illinois", "Springfield");
+var cCard1 = new clozeCard("Phoebe is the name of Justin's cat", "Phoebe");
+var cCard2 = new clozeCard("Chell is the character you play as in the Portal series", "Chell");
 
-var clozeQ = [clozeTest.partial, clozeTest2.partial];
-var clozeA = [clozeTest.cloze, clozeTest2.cloze];
+var clozeQ = [cCard1.partial, cCard2.partial];
+var clozeA = [cCard1.cloze, cCard2.cloze];
 
 // testing
 //console.log(clozeTest, clozeTest2);
@@ -51,8 +51,6 @@ var correctCount = 0;
 var cardCount = 0;
 
 function basicDeck() {
-    //console.log("Basic Flashcards: Answers are CASE SENSITIVE!");
-    //console.log("- - - - - - - - - - - ");
     //console.log(basicA.length);
     if(cardCount < basicA.length) {
         inquirer.prompt([
@@ -79,6 +77,37 @@ function basicDeck() {
         console.log("Here are the answers to the questions: ");
         for (var i = 0; i < basicA.length; i++) {
             console.log(i+1 + ": " + basicA[i]);
+        }
+    }
+}
+
+function clozeDeck() {
+    //console.log(clozeA.length);
+    if(cardCount < clozeA.length) {
+        inquirer.prompt([
+            {
+                name: 'question',
+                message: clozeQ[cardCount]
+            },
+        ]).then(function(answer) {
+            if(answer.question === clozeA[cardCount]) {
+                correctCount++;
+            } else {
+                wrongCount++;
+            }
+            cardCount++;
+            clozeDeck();
+        });
+    }
+    else {
+        console.log("");
+        console.log("Results: ");
+        console.log("Correct Answers: " + correctCount);
+        console.log("Wrong Answers: " + wrongCount);
+        console.log("- - - - - - - - - - - ");
+        console.log("Here are the answers to the questions: ");
+        for (var i = 0; i < clozeA.length; i++) {
+            console.log(i+1 + ": " + clozeA[i]);
         }
     }
 }
